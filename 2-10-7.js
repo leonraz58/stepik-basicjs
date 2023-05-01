cef14742bd43446e36d4cb3a65dd638ef0f5a30f
leonraz58 <leonraz58@gmail.com>
2023-05-01 22:58:01 +0300

    2-10-7

---
+++ 2-10-7.js
@@ -0,0 +1,20 @@
+const readline = require('readline');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+rl.on('line', (input) => {
+    let result;
+    let str = input;
+
+    // -- ваш код начинается тут
+
+    result = str;
+
+    // -- ваш код заканчивается тут
+
+    console.log(result);
+    rl.close();
+});
