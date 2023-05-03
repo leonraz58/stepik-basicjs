8cb1a75b94f1fd82d444072b8e6f64c9ebc09ac2
leonraz58 <leonraz58@gmail.com>
2023-05-03 22:47:28 +0300

    3-10-7

---
+++ 3-10-7.js
@@ -0,0 +1,20 @@
+const readline = require('readline');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+rl.on('line', (input) => {
+    let result;
+    let [x, y] = input.split(" ").map(Number);
+
+    // -- ваш код начинается тут
+
+    result = x > y ? 'x больше y' : 'y больше x'
+
+    // -- ваш код заканчивается тут
+
+    console.log(result);
+    rl.close();
+});
