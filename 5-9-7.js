09f6034c33462c923d26036a8cd5c9d358969915
leonraz58 <leonraz58@gmail.com>
2023-05-07 15:28:26 +0300

    5-9-7

---
+++ 5-9-7.js
@@ -0,0 +1,28 @@
+/*
+У вас есть переменная data, которая содержит входные пользовательские данные.
+
+Напишите JavaScript код, который считает сумму всех четных значений массива и записывает результат в переменную result.
+*/
+const readline = require('readline');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+rl.on('line', (input) => {
+    let result;
+    let data = JSON.parse(input);
+
+    // -- ваш код начинается тут
+    sum = 0;
+    for (num of data){
+        (num % 2 === 0) ? (sum+=num) : sum ;        
+    }
+    result = sum;
+
+    // -- ваш код заканчивается тут
+
+    console.log(result);
+    rl.close();
+});
